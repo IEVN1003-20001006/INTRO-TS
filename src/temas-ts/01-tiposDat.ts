6923428c041bca32960facbe3ae316a2a249ed06
Jazmin Ramirez <ramirez12corona@gmail.com>
2024-09-06 19:01:26 -0600

    primer commit

---
+++ src/temas-ts/01-tiposDat.ts
@@ -0,0 +1,19 @@
+
+var num1=38
+let num2=29
+const num3=10
+
+let nombre:string
+let num:number
+let activo:boolean
+
+nombre='Mario'
+num=32
+activo=true
+
+let matricula:number|string|boolean
+
+matricula=20001006
+matricula='asd056'
+let sum=num1+num2+num3
+console.log('el numero es',sum)
---
+++ src/temas-ts/02.interfaz.ts
@@ -0,0 +1,11 @@
+interface Alumno{
+    nombre:string;
+    edad:number;
+    email:string;
+    calificacion:number
+}
+const aumno:Alumno={
+    nombre:'mario',
+    edad:20,
+    email:'mario@gmail.com',
+}
